8a72bdb02877bfa69cb65a4875ac0e8f90b1290b
Jefferson Magalhães <jeffersoon.pm@gmail.com>
2019-09-03 12:19:12 -0300

    alteracoes fonte

---
+++ dist/forms-forms-module-es5.js
@@ -0,0 +1,69 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["forms-forms-module"],{
+
+/***/ "./src/app/pages/forms/forms.module.ts":
+/*!*********************************************!*\
+  !*** ./src/app/pages/forms/forms.module.ts ***!
+  \*********************************************/
+/*! exports provided: FormsModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "FormsModule", function() { return FormsModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _nebular_theme__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @nebular/theme */ "./node_modules/@nebular/theme/fesm5/index.js");
+/* harmony import */ var _theme_theme_module__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../@theme/theme.module */ "./src/app/@theme/theme.module.ts");
+/* harmony import */ var _forms_routing_module__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./forms-routing.module */ "./src/app/pages/forms/forms-routing.module.ts");
+/* harmony import */ var _forms_component__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./forms.component */ "./src/app/pages/forms/forms.component.ts");
+/* harmony import */ var _form_inputs_form_inputs_component__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./form-inputs/form-inputs.component */ "./src/app/pages/forms/form-inputs/form-inputs.component.ts");
+/* harmony import */ var _form_layouts_form_layouts_component__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./form-layouts/form-layouts.component */ "./src/app/pages/forms/form-layouts/form-layouts.component.ts");
+/* harmony import */ var _datepicker_datepicker_component__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./datepicker/datepicker.component */ "./src/app/pages/forms/datepicker/datepicker.component.ts");
+/* harmony import */ var _buttons_buttons_component__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./buttons/buttons.component */ "./src/app/pages/forms/buttons/buttons.component.ts");
+
+
+
+
+
+
+
+
+
+
+var FormsModule = /** @class */ (function () {
+    function FormsModule() {
+    }
+    FormsModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+            imports: [
+                _theme_theme_module__WEBPACK_IMPORTED_MODULE_3__["ThemeModule"],
+                _nebular_theme__WEBPACK_IMPORTED_MODULE_2__["NbInputModule"],
+                _nebular_theme__WEBPACK_IMPORTED_MODULE_2__["NbCardModule"],
+                _nebular_theme__WEBPACK_IMPORTED_MODULE_2__["NbButtonModule"],
+                _nebular_theme__WEBPACK_IMPORTED_MODULE_2__["NbActionsModule"],
+                _nebular_theme__WEBPACK_IMPORTED_MODULE_2__["NbUserModule"],
+                _nebular_theme__WEBPACK_IMPORTED_MODULE_2__["NbCheckboxModule"],
+                _nebular_theme__WEBPACK_IMPORTED_MODULE_2__["NbRadioModule"],
+                _nebular_theme__WEBPACK_IMPORTED_MODULE_2__["NbDatepickerModule"],
+                _forms_routing_module__WEBPACK_IMPORTED_MODULE_4__["FormsRoutingModule"],
+                _nebular_theme__WEBPACK_IMPORTED_MODULE_2__["NbSelectModule"],
+                _nebular_theme__WEBPACK_IMPORTED_MODULE_2__["NbIconModule"],
+            ],
+            declarations: [
+                _forms_component__WEBPACK_IMPORTED_MODULE_5__["FormsComponent"],
+                _buttons_buttons_component__WEBPACK_IMPORTED_MODULE_9__["ButtonsComponent"],
+                _form_inputs_form_inputs_component__WEBPACK_IMPORTED_MODULE_6__["FormInputsComponent"],
+                _form_layouts_form_layouts_component__WEBPACK_IMPORTED_MODULE_7__["FormLayoutsComponent"],
+                _datepicker_datepicker_component__WEBPACK_IMPORTED_MODULE_8__["DatepickerComponent"],
+            ],
+        })
+    ], FormsModule);
+    return FormsModule;
+}());
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=forms-forms-module-es5.js.map
